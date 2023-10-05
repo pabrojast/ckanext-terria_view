b93a571daf73c71057a6bd54d7fd73a8d9012240
Pablo Ignacio Rojas Torres <pablo.ignacio.rt@gmail.com>
2023-10-04 21:53:26 -0300

    Update plugin.py

--- ckanext/terria_view/plugin.py
+++ ckanext/terria_view/plugin.py
@@ -106,85 +106,125 @@ class Terria_ViewPlugin(plugins.SingletonPlugin):
         view_title = view.get('title', self.default_title)
         view_terria_instance_url = view.get('terria_instance_url', self.default_instance_url)
 
-        config = {  
+        # config = {  
+            # "version":"0.0.05",
+            # "initSources":[  
+                # {  
+                    # "catalog":[  
+                        # {  
+                            # "name":"User-Added Data",
+                            # "description":"The group for data that was added by the user via the Add Data panel.",
+                            # "isUserSupplied":True,
+                            # "id":"Root Group/User-Added Data",
+                            # "isOpen":True,
+                            # "type":"group"
+                        # },
+                        # {  
+                            # "name":"" + self.site_url + "",
+                            # "isUserSupplied":True,
+                            # "id":"Root Group/" + self.site_url + "",
+                            # "isOpen":True,
+                            # "url":"" + self.site_url + "",
+                            # "filterQuery":[  
+                                # SUPPORTED_FILTER_EXPR
+                            # ],
+                            # "groupBy":"organization",
+                            # "includeWms":True,
+                            # "includeWfs":True,
+                            # "includeKml":True,
+                            # "includeCsv":True,
+                            # "includeEsriMapServer":True,
+                            # "includeGeoJson":True,
+                            # "includeCzml":True,
+                            # "type":"ckan"
+                        # }
+                    # ]
+                # },
+                # {  
+                    # "sharedCatalogMembers":{  
+                        # "Root Group/User-Added Data":{  
+                            # "isOpen":True,
+                            # "type":"group",
+                            # "parents":[  
+
+                            # ]
+                        # },
+                        # "Root Group/" + self.site_url + "":{  
+                            # "isOpen":True,
+                            # "type":"ckan",
+                            # "parents":[  
+
+                            # ]
+                        # },
+                        # "Root Group/" + self.site_url + "/" + organization_id + "":{  
+                            # "isOpen":True,
+                            # "type":"group",
+                            # "parents":[  
+                                # "Root Group/" + self.site_url + ""
+                            # ]
+                        # },
+                        # "Root Group/" + self.site_url + "/" + organization_id + "/" + resource_id + "":{  
+                            # "nowViewingIndex":0,
+                            # "isEnabled":True,
+                            # "isShown":True,
+                            # "isLegendVisible":True,
+                            # "opacity":0.6,
+                            # "keepOnTop":False,
+                            # "disableUserChanges":False,
+                            # "tableStyle":{  
+                                # "scale":1,
+                                # "colorBinMethod":"auto",
+                                # "legendTicks":3,
+                                # "dataVariable":"id"
+                            # },
+                            # "type":"csv",
+                            # "parents":[  
+                                # "Root Group/" + self.site_url + "",
+                                # "Root Group/" + self.site_url + "/" + organization_id + ""
+                            # ]
+                        # }
+                    # }
+                # }
+            # ]
+        # }
+            config = {  
             "version":"0.0.05",
             "initSources":[  
                 {  
-                    "catalog":[  
-                        {  
-                            "name":"User-Added Data",
-                            "description":"The group for data that was added by the user via the Add Data panel.",
-                            "isUserSupplied":True,
-                            "id":"Root Group/User-Added Data",
-                            "isOpen":True,
-                            "type":"group"
+                
+                "catalog": [
+                        {
+                          "name": "data.dev-wins.com",
+                          "shareKeys": [
+                            "Root Group"
+                          ],
+                          "url": "https://pabrojast.github.io/prod.json",
+                          "type": "terria-reference",
+                          "isGroup": true
                         },
-                        {  
-                            "name":"" + self.site_url + "",
-                            "isUserSupplied":True,
-                            "id":"Root Group/" + self.site_url + "",
-                            "isOpen":True,
-                            "url":"" + self.site_url + "",
-                            "filterQuery":[  
-                                SUPPORTED_FILTER_EXPR
-                            ],
-                            "groupBy":"organization",
-                            "includeWms":True,
-                            "includeWfs":True,
-                            "includeKml":True,
-                            "includeCsv":True,
-                            "includeEsriMapServer":True,
-                            "includeGeoJson":True,
-                            "includeCzml":True,
-                            "type":"ckan"
-                        }
-                    ]
+                                {
+                              "name": "Power Stations (with template)",
+                              "type": "kml",
+                              "url": "https://data.dev-wins.com/dataset/9897691a-c6d4-416c-8d16-02e0e7db1a2f/resource/64abc581-d97b-45ae-bd2d-6bc6b2e5953d/download/bwa.kml",
+                              "cacheDuration": "5m",
+                              "featureInfoTemplate": {"template": "<h3>{{Station Name}} ({{DUID}})</h3><p><strong>{{Participant}}</strong><div><strong>{{Current Output (MW)}}MW</strong> at {{Most Recent Output Time (AEST)}}</div><div><strong>{{Current % of Reg Cap}}%</strong> of {{Reg Cap (MW)}}MW registered capacity</div><div><strong>{{Current % of Max Cap}}%</strong> of {{Max Cap (MW)}}MW maximum capacity</div></p><table><tbody><tr><td>Category</td><td class='strong'>{{Category}}</td></tr><tr><td>Classification</td><td class='strong'>{{Classification}}</td></tr><tr><td>Fuel Source</td><td class='strong'>{{Fuel Source - Primary}} ({{Fuel Source - Descriptor}})</td></tr><tr><td>Technology Type</td><td class='strong'>{{Technology Type - Primary}} ({{Technology Type - Descriptor}})</td></tr><tr><td>Physical Unit No.</td><td class='strong'>{{Physical Unit No_}}</td></tr><tr><td>Aggregation</td><td class='strong'>{{Aggregation}}</td></tr><tr><td>Unit Size (MW)</td><td class='strong'>{{Unit Size (MW)}}</td></tr><tr><td>Power generation</td><td><chart sources='http://services.aremi.nicta.com.au/aemo/v4/duidcsv/{{DUID}}?offset=1D,http://services.aremi.nicta.com.au/aemo/v4/duidcsv/{{DUID}}?offset=5D,http://services.aremi.nicta.com.au/aemo/v4/duidcsv/{{DUID}}?offset=30D,http://services.aremi.nicta.com.au/aemo/v4/duidcsv/{{DUID}}?offset=3M' source-names='1d,5d,30d,3m' downloads='http://services.aremi.nicta.com.au/aemo/v4/duidcsv/{{DUID}}?offset=1D,http://services.aremi.nicta.com.au/aemo/v4/duidcsv/{{DUID}}?offset=5D,http://services.aremi.nicta.com.au/aemo/v4/duidcsv/{{DUID}}?offset=30D,http://services.aremi.nicta.com.au/aemo/v4/duidcsv/{{DUID}}?offset=3M,http://services.aremi.nicta.com.au/aemo/v4/duidcsv/{{DUID}}?offset=1Y' download-names='1d,5d,30d,3m,1y' preview-x-label='Last 24 hours' column-names='Time,Power Generation' column-units='Date,MW'></chart></td></tr></tbody></table>"}
+                            }
+                          
+                      ]
+                    
                 },
-                {  
-                    "sharedCatalogMembers":{  
-                        "Root Group/User-Added Data":{  
-                            "isOpen":True,
-                            "type":"group",
-                            "parents":[  
-
-                            ]
-                        },
-                        "Root Group/" + self.site_url + "":{  
-                            "isOpen":True,
-                            "type":"ckan",
-                            "parents":[  
-
-                            ]
-                        },
-                        "Root Group/" + self.site_url + "/" + organization_id + "":{  
-                            "isOpen":True,
-                            "type":"group",
-                            "parents":[  
-                                "Root Group/" + self.site_url + ""
-                            ]
-                        },
-                        "Root Group/" + self.site_url + "/" + organization_id + "/" + resource_id + "":{  
-                            "nowViewingIndex":0,
-                            "isEnabled":True,
-                            "isShown":True,
-                            "isLegendVisible":True,
-                            "opacity":0.6,
-                            "keepOnTop":False,
-                            "disableUserChanges":False,
-                            "tableStyle":{  
-                                "scale":1,
-                                "colorBinMethod":"auto",
-                                "legendTicks":3,
-                                "dataVariable":"id"
-                            },
-                            "type":"csv",
-                            "parents":[  
-                                "Root Group/" + self.site_url + "",
-                                "Root Group/" + self.site_url + "/" + organization_id + ""
-                            ]
-                        }
-                    }
-                }
+                "homeCamera": {
+                            "north": 0,
+                            "east": 50,
+                            "south": -75,
+                            "west": 98
+                          },
+              "viewerMode": "3dSmooth",
+              "baseMaps": {
+                            "defaultBaseMapId": "basemap-positron",
+                            "previewBaseMapId": "basemap-positron"
+              }
+                
             ]
         }
         
